608a9dfb2ecaa5189b338b1d5bea7d796155f735
svecon <ond.svec@gmail.com>
2016-12-14 15:45:45 +0100

    q-learning algorithm with good defaults

--- hw13_q-learning/q_learning.py
+++ hw13_q-learning/q_learning.py
@@ -13,15 +13,15 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--env", default="Taxi-v1", type=str, help="Name of the environment.")
-    parser.add_argument("--episodes", default=1000, type=int, help="Episodes in a batch.")
-    parser.add_argument("--max_steps", default=500, type=int, help="Maximum number of steps.")
+    parser.add_argument("--episodes", default=3000, type=int, help="Episodes in a batch.")
+    parser.add_argument("--max_steps", default=1000, type=int, help="Maximum number of steps.")
     parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
 
-    parser.add_argument("--alpha", default=0.1, type=float, help="Learning rate.")
-    parser.add_argument("--alpha_final", default=0, type=float, help="Learning rate decay rate.")
-    parser.add_argument("--epsilon", default=0.5, type=float, help="Epsilon.")
-    parser.add_argument("--epsilon_final", default=0.01, type=float, help="Epsilon decay rate.")
-    parser.add_argument("--gamma", default=1.0, type=float, help="Discounting factor.")
+    parser.add_argument("--alpha", default=0.5, type=float, help="Learning rate.")
+    parser.add_argument("--alpha_decay", default=0.000001, type=float, help="Learning rate decay rate.")
+    parser.add_argument("--epsilon", default=0.1, type=float, help="Epsilon.")
+    parser.add_argument("--epsilon_decay", default=0.00001, type=float, help="Epsilon decay rate.")
+    parser.add_argument("--gamma", default=0.9999, type=float, help="Discounting factor.")
     args = parser.parse_args()
 
     # Create the environment
@@ -31,6 +31,7 @@ if __name__ == "__main__":
     Q = np.zeros([env.states, env.actions])
     epsilon = args.epsilon
     alpha = args.alpha
+    gamma = args.gamma
     episode_rewards, episode_lengths = [], []
 
     for episode in range(args.episodes):
@@ -41,13 +42,17 @@ if __name__ == "__main__":
             if args.render_each and episode > 0 and episode % args.render_each == 0:
                 env.render()
 
-            # TODO: compute action using epsilon-greedy policy
-            # action = ...
+            # Compute action using epsilon-greedy policy
+            if np.random.random() < epsilon:
+                action = np.random.randint(0, env.actions)
+            else:
+                action = np.argmax(Q[state])
 
             next_state, reward, done, _ = env.step(action)
             total_reward += reward
 
             # Update Q
+            Q[state][action] = Q[state][action] + alpha*( reward + ( gamma*np.max(Q[next_state]) ) - Q[state][action] )
 
             state = next_state
             if done:
@@ -59,7 +64,7 @@ if __name__ == "__main__":
             print("Episode {}, mean 100-episode reward {}, mean 100-episode length {}, epsilon {}, alpha {}.".format(
                 episode + 1, np.mean(episode_rewards[-100:]), np.mean(episode_lengths[-100:]), epsilon, alpha))
 
-        if args.epsilon_final:
-            epsilon = np.exp(np.interp(episode + 1, [0, args.episodes], [np.log(args.epsilon), np.log(args.epsilon_final)]))
-        if args.alpha_final:
-            alpha = np.exp(np.interp(episode + 1, [0, args.episodes], [np.log(args.alpha), np.log(args.alpha_final)]))
+        if args.epsilon_decay:
+            epsilon = np.exp(np.interp(episode + 1, [0, args.episodes], [np.log(args.epsilon), np.log(args.epsilon_decay)]))
+        if args.alpha_decay:
+            alpha = np.exp(np.interp(episode + 1, [0, args.episodes], [np.log(args.alpha), np.log(args.alpha_decay)]))
